72f031f55e95ff8c1fb0b4bff13825f46fa466dd
mozeal <mozeal@gmail.com>
2017-06-02 06:56:10 +0700

    Template

---
+++ sw-precache-config.js
@@ -0,0 +1,34 @@
+/**
+ * @license
+ * Copyright (c) 2016 The Polymer Project Authors. All rights reserved.
+ * This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+ * The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+ * The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+ * Code distributed by Google as part of the polymer project is also
+ * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+ */
+
+/* eslint-env node */
+
+module.exports = {
+    staticFileGlobs: [
+        '/index.html',
+        '/manifest.json',
+        '/images/*',
+        '/bower_components/webcomponentsjs/*',
+    ],
+    navigateFallback: 'index.html',
+    navigateFallbackWhitelist: [/^(?!.*\.html$|\/data\/).*/],
+    runtimeCaching: [
+        {
+            urlPattern: /.*\.(png|jpg|gif|svg)/i,
+            handler: 'fastest',
+            options: {
+                cache: {
+                    maxEntries: 200,
+                    name: 'data-images-cache'
+                }
+            }
+        }
+    ]
+};
